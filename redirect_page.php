f467a1bd9563b5c0c5e856f1e04dfb44e41b854e
jazinheira <jazinheira@github.com>
2015-06-12 22:19:47 -0400

    Update redirect_page to redirect to CAS
    This will build CAS link that has the service set to the previous page.
    Users should be returned to their previous page once they log in
    instead of being redirected to the index page.

--- redirect_page.php
+++ redirect_page.php
@@ -15,7 +15,15 @@ $PAGE->set_title("Redirect Page");
 $PAGE->set_heading("Redirect...");
 $PAGE->set_url($CFG->wwwroot.'/redirect_page.php');
 
-$redirecturl = get_config("block_visitor_login", "redirecturl");	
+//$casurl is the CAS host url. Cannot have trailing slash.
+$casurl = get_config("block_visitor_login", "casurl"); 
+$referredurl = $_SERVER['HTTP_REFERER'];
+
+// To build the redirect url, take the CAS host url, append '/login?service='
+// and the escaped url of the previous page.
+// This will redirect the user back to the same page once they are logged in.
+$redirecturl = $casurl . '/login?service=' . urlencode($referredurl);
+
 echo $OUTPUT->header();
 redirect($redirecturl,get_string('redirectmessage', 'block_visitor_login'), 5);
 echo $OUTPUT->footer();
